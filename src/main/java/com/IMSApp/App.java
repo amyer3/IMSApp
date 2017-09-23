2aec3d8955041fc169faa0bb505d8a73e724bbad
amyer3 <aj.myers93@gmail.com>
2017-09-23 11:23:33 -0700

    initial vcs

--- src/main/java/com/IMSApp/App.java
+++ src/main/java/com/IMSApp/App.java
@@ -1,13 +1,16 @@
 package com.IMSApp;
 
+import java.sql.Connection;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws java.sql.SQLException {
+        Connection maindb = null;
+        maindb = DBHandler.connect();
+        System.out.println(maindb.getMetaData());
     }
 }
--- src/main/java/com/IMSApp/DBHandler.java
+++ src/main/java/com/IMSApp/DBHandler.java
@@ -5,16 +5,7 @@ import java.sql.*;
 public class DBHandler {
     public static String DBloc = "jdbc:hsqldb:file:maindb";
 
-    public static void main(String[] args) throws ClassNotFoundException {
-        Connection maindbCon = null;
-        try {
-            maindbCon = DriverManager.getConnection(DBloc);
-            System.out.println("database connected");
-            System.out.println(maindbCon.getMetaData());
-        } catch (java.sql.SQLException e) {
-            System.out.println("error initializing database");
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
     }
     public static void pullData(){
         ResultSet rs = null;
@@ -25,6 +16,29 @@ public class DBHandler {
             DriverManager.getConnection("jdbc:hsqldb:file:maindb;shutdown=true");
         } catch (java.sql.SQLException e){
             System.out.println("could not shut down properly");
+        }
+    }
+
+    public static Connection connect(){
+        Connection maindbCon = null;
+        try {
+            maindbCon = DriverManager.getConnection(DBloc);
+            System.out.println("database connected");
+            System.out.println(maindbCon.getMetaData());
+        } catch (java.sql.SQLException e) {
+            System.out.println("error initializing database");
+            e.printStackTrace();
+        }
+        return maindbCon;
+    }
+
+    public static void destroy(Connection c){
+        try {
+            PreparedStatement psmt = c.prepareStatement("DROP SCHEMA PUBLIC CASCADE");
+            psmt.executeQuery();
+            System.out.println("database destroyed");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
