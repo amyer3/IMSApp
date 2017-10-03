fde413f07032b56c8179df900d617b3b386ebba0
amyer3 <aj.myers93@gmail.com>
2017-10-02 19:08:17 -0700

    GUI

--- src/main/java/IMSApp/sGUI.java
+++ src/main/java/IMSApp/sGUI.java
@@ -6,18 +6,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class sGUI {
-    private JLabel outputText;
-    private JFrame cards;
-    private JButton back;
-    private GridLayout layout = new GridLayout(0,1);
-    private CardLayout cardPattern = new CardLayout();
+    JLabel outputText;
+    JFrame cards;
+    JButton back;
+    GridLayout layout = new GridLayout(0,1);
+    CardLayout cardPattern = new CardLayout();
+    JPanel superPanel;
 
     public static void main(String[] args) {
     }
 
     public void doGui(){
         cards = new JFrame();
+        Container panel = new Container();
+        cards.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JPanel home = new JPanel();
+        superPanel = new JPanel(new CardLayout());
 
         cards.setSize(400, 600);
 
@@ -44,13 +48,17 @@ public class sGUI {
         exportButton.addActionListener(new ButtonClickListener());
 
         outputText = new JLabel("", SwingConstants.CENTER);
+
         JPanel addCard = new JPanel();
+        JTextField idField = new JTextField();
+        addCard.add(idField);
         JPanel salesCard = new JPanel();
         JPanel updateCard = new JPanel();
         JPanel viewCard = new JPanel();
         JPanel exportCard = new JPanel();
-        this.cards.setLayout(cardPattern);
-        this.cards.setSize(800, 500);
+
+        cards.setLayout(cardPattern);
+        cards.setSize(800, 500);
         home.setLayout(layout);
         home.add(tLabel);
         home.add(madeButton);
@@ -59,20 +67,25 @@ public class sGUI {
         home.add(updateButton);
         home.add(exportButton);
         home.add(outputText);
-        this.cards.add(home);
-        this.cards.add(addCard);
-        this.cards.add(salesCard);
-        this.cards.add(updateCard);
-        this.cards.add(viewCard);
-        this.cards.add(exportCard);
-        this.cards.setVisible(true);
+
+        superPanel.add(home, "home");
+        superPanel.add(addCard, "add");
+        superPanel.add(salesCard, "sales");
+        superPanel.add(updateCard, "update");
+        superPanel.add(viewCard, "view");
+        superPanel.add(exportCard, "export");
+
+        cards.add(superPanel, BorderLayout.CENTER);
+        cards.pack();
+        cards.setVisible(true);
 
     }
     private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e){
+            CardLayout c1 = (CardLayout) superPanel.getLayout();
             String command = e.getActionCommand();
             if(command.equals( "made")){
-                newRecord();
+                c1.show(superPanel, "add");
             } else if(command.equals("sold")){
                 saleRecord();
             } else if(command.equals( "view")){
